437c78d41a3e3ee462be83af42e784712f3b7ab5
Miguel <mbouchan@resuelve.mx>
2018-03-06 10:42:57 -0600

    deploy 7

--- general_prueba/handler.js
+++ general_prueba/handler.js
@@ -1,9 +1,9 @@
 'use strict';
 var moment = require('moment');
 
-module.exports.handler = function(event, context, cb) {
+module.exports.handler = function (event, context, cb) {
 
   return cb(null, {
-    message: 'la horas bb otro esta: '+moment().format()
+    message: 'la hora es: ' + moment().format()
   });
 };
