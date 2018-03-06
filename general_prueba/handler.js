4912e87b63eba1bc10f49b37c39ad01097af7e4a
Miguel <mbouchan@resuelve.mx>
2018-03-05 18:37:31 -0600

    agregado package

--- general_prueba/handler.js
+++ general_prueba/handler.js
@@ -4,6 +4,6 @@ var moment = require('moment');
 module.exports.handler = function(event, context, cb) {
 
   return cb(null, {
-    message: 'la hora es: '+moment().format()
+    message: 'la hora esta: '+moment().format()
   });
 };
